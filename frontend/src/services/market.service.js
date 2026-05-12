e0e3882caa05b45108787104a1ca9e60c407b975
Muhammed Emin Aldaş <129961173+eminaldas@users.noreply.github.com>
2026-05-12 17:59:35 +0300

    feat(market): market service getStocks + savePrefs

--- frontend/src/services/market.service.js
+++ frontend/src/services/market.service.js
@@ -5,6 +5,16 @@ class MarketService {
         const res = await axiosInstance.get('/market/rates');
         return res.data;
     }
+
+    static async getStocks() {
+        const res = await axiosInstance.get('/market/stocks');
+        return res.data;
+    }
+
+    static async savePrefs(tickers) {
+        const res = await axiosInstance.put('/market/preferences', { tickers });
+        return res.data;
+    }
 }
 
 export default MarketService;
